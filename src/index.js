6613ca2159825291293e7ba26f9a7adb798785ef
YennyCastillo <aricrisf@hotmail.com>
2018-05-24 17:54:05 -0500

    Cambios html y Js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+//eventos del DOM
+const buttonC= document.getElementById("botonE");
+buttonC.addEventListener("click" , window.cipher.encode);
+
+const buttonD= document.getElementById("botonD");
+buttonD.addEventListener("click" ,window.cipher.decode);
+
+
+
+let text= document.getElementById("texto");
+text.addEventListener("input" , ()=>
+{});
+
+let clave= document.getElementById("llave");
+clave.addEventListener("input" , ()=>
+{});
