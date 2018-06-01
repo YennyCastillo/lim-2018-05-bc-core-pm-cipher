ac774d5ab521a8c8d10bc858365e33ff32acce76
YennyCastillo <aricrisf@hotmail.com>
2018-06-01 16:19:37 -0500

    subiendo proyecto final

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,23 @@
-const encriptar = () => {
-  let encode= document.getElementById('texto').value;
-  let key1= parseInt(document.getElementById('llave').value);
-  let longTex = encode.length;
-  let i = 0;
-
-  document.getElementById("answer").innerHTML = "";
-  for (i=0 ; i < longTex ; i++)
+const encriptar = (string, offset) => {
+  let answerE="";
+  for (let i=0 ; i < string.length ; i++)
   {
-    let stringE= (encode.toUpperCase().charAt(i)).charCodeAt(0);
-    let answerE = String.fromCharCode((stringE-65+key1) % 26 +65);
-    console.log(answerE);
-    document.getElementById("answer").innerHTML +=answerE;
+    let stringE= string.toUpperCase().charCodeAt(i);
+    answerE += String.fromCharCode((stringE-65+offset) % 26 +65);
   }
+  return answerE;
 }
 
-const desencriptar = () => {
-  let decode= document.getElementById('texto').value;
-  let key2= parseInt(document.getElementById('llave').value);
-  let longText = decode.length;
-  let i = 0;
-document.getElementById("answer").innerHTML = "";
-  for (i=0 ; i < longText ; i++)
+const desencriptar = (string, offset) => {
+  let answerD = "";
+  for (let i=0 ; i < string.length ; i++)
   {
-    let stringD= (decode.toUpperCase().charAt(i)).charCodeAt(0);
-    let answerD= String.fromCharCode((stringD+65-key2) % 26 +65);
-    console.log(answerD);
-    document.getElementById("answer").innerHTML += answerD;
-   }
-
+    let stringD= string.toUpperCase().charCodeAt(i);
+    answerD += String.fromCharCode((stringD+65-offset) % 26 +65);
+  }
+  return answerD;
 }
 window.cipher = {
   encode : encriptar,
   decode : desencriptar
-
 };
-
-
-/*hola mund*/
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-
-//eventos del DOM
 const buttonC= document.getElementById("botonE");
-buttonC.addEventListener("click" , window.cipher.encode);
+buttonC.addEventListener("click" , () =>{
+let encode= document.getElementById("texto").value;
+let key1= parseInt(document.getElementById('llave').value);
+let result= document.getElementById("answer");
+result.innerHTML = window.cipher.encode(encode,key1);});
+
 
 const buttonD= document.getElementById("botonD");
-buttonD.addEventListener("click" ,window.cipher.decode);
-
-
-
-let text= document.getElementById("texto");
-text.addEventListener("input" , ()=>
-{ console.log(text.value);
-})
-
-let clave= document.getElementById("llave");
-clave.addEventListener("input" , ()=>
-{});
+buttonD.addEventListener("click" , () =>{
+let decode= document.getElementById('texto').value;  let key2= parseInt(document.getElementById('llave').value);
+let result= document.getElementById("answer");
+result.innerHTML = window.cipher.decode(decode,key2);});
+const buttonS = document.getElementById("botonS");
+buttonS.addEventListener("click" , ()=>{
+document.getElementById("root").style.display = "block";
+document.getElementById("inicio").style.display = "none";
+});
